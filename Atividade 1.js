9f994db766e151b2f1c06e4a5a8757593248eccb
ZaTrooX <jianpalini11@gmail.com>
2025-06-03 17:19:22 -0300

    Create Atividade 1.js

---
+++ Atividade 1.js
@@ -0,0 +1,13 @@
+//let primeiroNome = prompt("Qual é seu primeiro nome?")
+//let sobrenome = prompt("Qual é seu sobrenome?")
+//let idade = Number(prompt ("Qual sua idade?"))
+//let estudante = true
+
+let primeiroNome = "Jian"
+let sobrenome = "Palini"
+let idade = 26
+let estudante = true
+
+
+console.log("Meu nome é", primeiroNome,sobrenome, "e eu tenho", idade, "anos", "e sou estudante", estudante)
+
