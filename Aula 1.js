105bba84afeebd3f67f0c05ff592bd20ff9758c7
ZaTrooX <jianpalini11@gmail.com>
2025-06-03 16:55:38 -0300

    Update Aula 1.js

--- Aula 1.js
+++ Aula 1.js
@@ -11,7 +11,9 @@
 const nome = "Patolino da Silva"
 const idade = 4000
 
+let nome2 = "Perna Longa Curta Perna"
+let idade2 = 5
 
-console.log("Olá!", "Meu nome é ", nome, "e eu tenho", idade, "anos")
+
+console.log("Olá!", "Meu nome é ", nome2, "e eu tenho", idade2, "anos")
 // Olá" Meu nome é Jian e eu tenho 26 anos
-
