797f47c074696371efed69471a12a8698af7da87
JianLPalini <jianpalini11@gmail.com>
2025-06-04 17:07:37 -0300

    Create AtividadeVariavel4.js

---
+++ AtividadeVariavel4.js
@@ -0,0 +1,9 @@
+const nomeRobo = "Megazorde"
+const passosdeDança = "Vaneira"
+const estaDançando = true
+
+
+console.log("O", nomeRobo, "já aprendeu", passosdeDança, "passos! Ele está dançando agora ?", estaDançando)
+
+console.log(typeof nomeRobo)
+console.log(typeof estaDançando)
