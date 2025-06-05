9aeb5b4f1fdabc1c3407f7e77f6fdda32e70702c
JianLPalini <jianpalini11@gmail.com>
2025-06-05 17:21:59 -0300

    Update Atividade1.js

--- Comparadores/Atividade1.js
+++ Comparadores/Atividade1.js
@@ -1,1 +1,98 @@
+// Comparadores
+// São eles:
+// Igual: ===
+// Diferente: !==
+// Maior e maior igual: > e >=
+// Menor e menor igual: < e <=
+
+
+
+
+// > e >=
+
+// 1 > 2  // false, porque 1 é menor que 2
+// 2 > 2  // false, porque 2 é igual a 2
+// 3 > 2  // true, porque 3 é maior que 2
+
+// 1 >= 2 // false, porque 1 é menor que 2
+// 2 >= 2 // false, porque 2 é igual a 2
+// 3 >= 2 // true, porque 3 é maior que 2
+
+
+
+
+// < e <=
+
+// 1 < 2  // true, porque 1 é menor que 2
+// 2 < 2  // false, porque 2 é igual a 2
+// 3 < 2  // false, porque 3 é maior que 2
+
+// 1 <= 2 // true, porque 1 é menor que 2
+// 2 <= 2 // true, porque 2 é igual a 2
+// 3 <= 2 // false, porque 3 é maior que 2
+
+verificando = (12!==3)
+console.log(verificando)
+
+let n1 = 3
+let n2 = 2
+let n3 = 4
+let n4 = "3"
+
+let igual = n1===n1
+let diferente = n2!==n1
+let maiorIgual = n1>=n3
+let menorIgual = n2<=n3
+let igual1 = n1===n4
+let diferente1 = n1!==n4
+
+console.log(igual)
+console.log(diferente)
+console.log(maiorIgual)
+console.log(menorIgual)
+console.log(igual1)
+console.log(diferente1)
+
+
+let animal = "gato"
+let animal2 = "Gato"
+
+let igualGato = ("gato"==="Gato")
+
+console.log(igualGato)
+
+let condição = true
+let condição2 = false
+
+let condiçãoFinal = (condição!==condição2)
+
+console.log(condiçãoFinal)
+
+let number1 = 5
+let number2 = '5'
+
+let igualNumber = 5==='5'
+
+console.log(igualNumber)
+
+let nome = 'maria'
+let nome2 = 'marea'
+
+let igualNome = 'maria'==='marea'
+
+console.log(igualNome)
+
+let jogo = (7>=7)
+let jogo1 = (7<=9)
+let jogo2 = (10!==10)
+let jogo3 = (5<3)
+let jogo4 = (2<1)
+let jogo5 = (true!==true)
+
+console.log(jogo)
+console.log(jogo1)
+console.log(jogo2)
+console.log(jogo3)
+console.log(jogo4)
+console.log(jogo5)
 
