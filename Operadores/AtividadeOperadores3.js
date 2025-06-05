ba777ea31fd1cbbaee00426cf8caf06fe9ee9d0e
JianLPalini <jianpalini11@gmail.com>
2025-06-05 14:42:28 -0300

    Create AtividadeOperadores3.js

---
+++ Operadores/AtividadeOperadores3.js
@@ -0,0 +1,45 @@
+// Notas aluno Joao
+let numeroJoao1 = 2.5
+let numeroJoao2 = 4
+let numeroJoao3 = 6
+let mediaJoao = ((numeroJoao1+numeroJoao2+numeroJoao3)/3)
+
+// Notas aluno Robersvaldo
+let numeroRob1 = 7.5
+let numeroRob2 = 4.8
+let numeroRob3 = 6.3
+let mediaRob = ((numeroRob1+numeroRob2+numeroRob3)/3)
+
+// Notas aluna Maria
+let numeroMaria1 = 3.5
+let numeroMaria2 = 8
+let numeroMaria3 = 6
+let mediaMaria = ((numeroMaria1+numeroMaria2+numeroMaria3)/3)
+
+// Notas aluno Julian
+let numeroJul1 = 5
+let numeroJul2 = 8
+let numeroJul3 = 8
+let mediaJul = ((numeroJul1+numeroJul2+numeroJul3)/3)
+
+// Notas aluno Maike
+let numeroMaike1 = 8.5
+let numeroMaike2 = 7
+let numeroMaike3 = 6.65
+let mediaMaike = ((numeroMaike1+numeroMaike2+numeroMaike3)/3)
+
+// ".toFixed(2) ou (3) " serve para fixar a casa decimal depois da virgula.
+console.log("Aluno Joao tirou a nota:", mediaJoao.toFixed(2))
+console.log("Aluno Robersvaldo tirou a nota:", mediaRob.toFixed(2))
+console.log("Aluna Maria tirou a nota:", mediaMaria.toFixed(2))
+console.log("Aluno Julian tirou a nota:", mediaJul.toFixed(2))
+console.log("Aluno Maike tirou a nota:", mediaMaike.toFixed(2))
+
+
+// Ex.3
+
+let idadeHumano = 1
+let idadeDog = 7
+let idadeTotal = idadeHumano*idadeDog
+
+console.log("Eu teria a idade de Dog de:", idadeTotal)
