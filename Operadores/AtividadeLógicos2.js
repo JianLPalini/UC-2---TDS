903f5dc8c2c559d4d8c6f0a9551cd9d3024db1c6
JianLPalini <jianpalini11@gmail.com>
2025-06-10 14:56:28 -0300

    Create AtividadeLógicos2.js

---
+++ Operadores/AtividadeLógicos2.js
@@ -0,0 +1,14 @@
+let temAlcance = true
+let temSenha = false
+
+let acesso = (temAlcance == true)  && (temSenha == true)
+
+console.log(acesso)
+
+
+let renda = 2100
+let nomeLimpo = true
+
+let aprovadoEmprestimo = ((renda > 2000) && (nomeLimpo == true))
+
+console.log(aprovadoEmprestimo)
