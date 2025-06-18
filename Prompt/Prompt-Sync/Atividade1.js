45804a2aafc29e50bb087f78d142511730f83172
JianLPalini <jianpalini11@gmail.com>
2025-06-18 17:21:46 -0300

    Create Atividade1.js

---
+++ Prompt/Prompt-Sync/Atividade1.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+let numero1 = parseFloat(prompt("Coloque o Número 1:"))
+let numero2 = parseFloat(prompt("Coloque o Número 2:"))
+let soma = (numero1+numero2)
+
+console.log(`Soma: ${soma}`)
