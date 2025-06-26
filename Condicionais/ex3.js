37623b57d643784e232cc4b3f519cf2405435f23
JianLPalini <jianpalini11@gmail.com>
2025-06-26 14:37:30 -0300

    Create ex3.js

---
+++ Condicionais/ex3.js
@@ -0,0 +1,10 @@
+function podeEntrar(){
+    let idade = Number(prompt("Digite sua idade: "))
+    if(idade >= 16){
+        alert("Pode entrar no cinema!")
+    }else{
+        alert("Vai embora pra casa!")
+    }
+
+}
+podeEntrar()
