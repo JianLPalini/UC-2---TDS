54a63b490c5f7bbf54deaf4e5d344dc2b0d7b1cf
JianLPalini <jianpalini11@gmail.com>
2025-06-26 15:31:58 -0300

    Update DesafioGamer.js

--- Condicionais/DesafioGamer.js
+++ Condicionais/DesafioGamer.js
@@ -26,3 +26,25 @@ function escolherPersonagem(){
         }
 }
 escolherPersonagem()
+
+
+// OUTRA OPÇÃO SERIA
+
+function escolherPersonagem(){
+    let escolhaClasse = prompt("Informe a classe do Herói que você deseja: ")
+    // Pede ao usuário o nível e converte tbm sua resposta
+    let nível = prompt("Qual nível do seu personagem: ")
+    // Verificando se o personagem é guerreiro e o nível é 50 ou mais
+    if(escolhaClasse === "Guerreiro" && nível >= 50) {
+            alert(`Você é um guerreiro lendário! Classe: ${escolharClasse}`)
+        }else if(escolhaClasse === "mago" && nível >= 50){
+            alert("Você domina a magia suprema meu querido!")
+        }else if(escolhaClasse === "Arqueiro" && nível >= 50){
+            alert("Você é o mestre das flechas!")
+        } // Caso nenhum das condições acima seja verdadeira
+        else{
+            alert("CONTINUE UPANDO SEU NOOB!!!!")
+        }
+            
+}
+escolherPersonagem()
