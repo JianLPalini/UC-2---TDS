792496fd8922f4f61397dee2e96b5feaf4d09d08
JianLPalini <jianpalini11@gmail.com>
2025-07-02 17:22:44 -0300

    Create script.js

---
+++ CardPerguntas/script.js
@@ -0,0 +1,10 @@
+function mostrarPerguntas(tipo){
+    //variável para guardar a pergunta que será exibida
+    let pergunta =""
+    // verificando o tipo de habilidade escolhida
+    if(tipo === "soft"){
+        pergunta = " Aqui é com vcs!"
+    }else if(tipo === "hard"){
+        pergunta = " Aqui é com vcs!"
+    }
+}
