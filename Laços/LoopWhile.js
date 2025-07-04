05102b1fa1bb7f933914d392855041f152a4653b
JianLPalini <jianpalini11@gmail.com>
2025-07-04 16:31:34 -0300

    Create LoopWhile.js

---
+++ Laços/LoopWhile.js
@@ -0,0 +1,18 @@
+// While - Enquanto
+
+// Equanto uma condição for verdadeira, ele executa o meu código
+
+let voltas = 0;
+while (voltas <= 10){
+    console.log(`Você já deu ${voltas} voltas.`)
+    //voltas = voltas +1
+    //voltas += 1
+    voltas ++
+}
+console.log("Parabens! Você completou a corrida!")
+
+let segundos = 10;
+while (segundos >= 0){
+    console.log(segundos)
+    segundos --
+}
