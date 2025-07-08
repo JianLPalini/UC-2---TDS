4249d185e8eebe8cd24204fa14b840fd12fb2295
JianLPalini <jianpalini11@gmail.com>
2025-07-08 16:57:48 -0300

    Create ex2.js

---
+++ OBJETOS/ex2.js
@@ -0,0 +1,8 @@
+let alienTurista = {
+    nomeGalactico: 'FungadoLox',
+    planetaDeOrigem: 'Alfa Centaure',
+    coisasQueTrouxe: ["Capivara de 3 cabeças", "Video dançando vaneira na nave", "Uma estrela no potinho", "Dedo do Lula"],
+    assustadoCom: ("Brasileiros", "Mosquito da dengue", "A cara do Sid", "Com a pontualidade do Kalleo" ),
+    falaPortugues: false
+}
+console.log(alienTurista)
