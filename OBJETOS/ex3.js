ccb913209bc114d7817a1e02b04af2aeb1ea0e07
JianLPalini <jianpalini11@gmail.com>
2025-07-08 17:21:55 -0300

    Create ex3.js

---
+++ OBJETOS/ex3.js
@@ -0,0 +1,12 @@
+const filme = {
+    nomeFilme: "John Wick - De volta ao Jogo",
+    direção: "Chad Stahelski",
+    anoLançamento: 2014,
+    elenco: ["Keanu Reeves", "Ian McShane", "Lance Reddick", "John Leguizamo", "Alfie Owen-Allen", "Michael Nyqvist", "Adrianne Palicki", "Willem Dafoe", "Daniel Bernhardt", "Scott Adkins"],
+    assistiu: true
+}
+console.log(filme.nomeFilme)
+console.log(filme.direção)
+console.log(filme.assistiu)
+console.log(filme['elenco'])
+console.log(filme['assistiu'])
