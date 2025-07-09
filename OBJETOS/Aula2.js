b3358c78e42e033414530ee4933b625def1bbe6f
JianLPalini <jianpalini11@gmail.com>
2025-07-09 16:24:34 -0300

    Update Aula2.js

--- OBJETOS/Aula2.js
+++ OBJETOS/Aula2.js
@@ -1,8 +1,39 @@
-const cachorro = {
+/*const cachorro = {
     nome: "Bolt",
     raca: "Vira-lata",
     latir: function() {
         console.log("Au au!");
     }
 }
-cachorro.latir(); // chama o método
+cachorro.latir(); // chama o método */
+
+/* const donoDoPet = {
+    nome1: "Vitor Hugo",
+    pet: {
+        nomeDoPet: "Wanda",
+        raca: "Vira-lata",
+        idade: 1
+    }
+}
+console.log(donoDoPet.pet.nomeDoPet)
+console.log(donoDoPet.nome1) */
+
+
+// Acessando arrays dentro de objetos {[]}
+
+/*const curso = {
+    nome: "Noturno Frontend",
+    linguagens: ["JS", "CSS", "HTML"]
+}
+console.log(curso.linguagens[0]) */
+
+
+// Array de objetos [{}]
+
+const professores = [
+    {nome: "Andrei", modulo: 1},
+    {nome: "Vitor", modulo: 2},
+    {nome: "Mina", modulo: 3}
+]
+console.log(professores[1].nome)
+
