fa5ab6ab928496961e92e7923769cb90b52fbe27
JianLPalini <jianpalini11@gmail.com>
2025-07-09 16:38:05 -0300

    Create ex8.js

---
+++ OBJETOS/ex8.js
@@ -0,0 +1,20 @@
+const filme = {
+    nomeFilme: "John Wick - De volta ao Jogo",
+    direção: "Chad Stahelski",
+    anoLançamento: 2014,
+    elenco: ["Keanu Reeves", "Ian McShane", "Lance Reddick", "John Leguizamo", "Alfie Owen-Allen", "Michael Nyqvist", "Adrianne Palicki", "Willem Dafoe", "Daniel Bernhardt", "Scott Adkins"],
+    assistiu: true,
+    nomePersonagem: ["John Wick", "Winston", "Charon", "Aurelio", "Iosef Tarasov", "Viggo Tarasov", "Ms. Perkins", "Marcus", "Kirill", "Scott"]
+}
+console.log(filme.nomeFilme)
+console.log(filme.direção)
+console.log(filme.assistiu)
+console.log(filme['elenco'])
+console.log(filme['assistiu'])
+
+console.log(filme.elenco[0],filme.nomePersonagem[0])
+console.log(filme.elenco[1],filme.nomePersonagem[1])
+console.log(filme.elenco[2],filme.nomePersonagem[2])
+console.log(filme.elenco[3],filme.nomePersonagem[3])
+filme.elenco[0]= "Xuxa"
+console.log(filme)
