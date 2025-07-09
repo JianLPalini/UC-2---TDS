ba6955d19cbcbbb4a21cd3ee2c52d4e694c15b8b
JianLPalini <jianpalini11@gmail.com>
2025-07-09 14:18:39 -0300

    Create ex4.js

---
+++ OBJETOS/ex4.js
@@ -0,0 +1,6 @@
+const pessoa = {
+    nome: "Dean Winchester",
+    idade: 32,
+    gêneroMusical: "Rock"
+}
+console.log("O nome da pessoa é " + pessoa.nome + ", ela tem " + pessoa.idade + " anos e gosta muito de " + pessoa.gêneroMusical)
