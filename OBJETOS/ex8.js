6a215f4485cbebfc9d4c1e1a9051acad6b8f2a4b
JianLPalini <jianpalini11@gmail.com>
2025-07-09 16:50:20 -0300

    Update ex8.js

--- OBJETOS/ex8.js
+++ OBJETOS/ex8.js
@@ -6,15 +6,15 @@ const filme = {
     assistiu: true,
     nomePersonagem: ["John Wick", "Winston", "Charon", "Aurelio", "Iosef Tarasov", "Viggo Tarasov", "Ms. Perkins", "Marcus", "Kirill", "Scott"]
 }
-console.log(filme.nomeFilme)
+/*console.log(filme.nomeFilme)
 console.log(filme.direção)
 console.log(filme.assistiu)
 console.log(filme['elenco'])
-console.log(filme['assistiu'])
+console.log(filme['assistiu'])*/
 
-console.log(filme.elenco[0],filme.nomePersonagem[0])
-console.log(filme.elenco[1],filme.nomePersonagem[1])
-console.log(filme.elenco[2],filme.nomePersonagem[2])
-console.log(filme.elenco[3],filme.nomePersonagem[3])
+console.log(`${filme.elenco[0]} interpreta ${filme.nomePersonagem[0]}`)
+console.log(`${filme.elenco[1]} interpreta ${filme.nomePersonagem[1]}`)
+console.log(`${filme.elenco[2]} interpreta ${filme.nomePersonagem[2]}`)
+console.log(`${filme.elenco[3]} interpreta ${filme.nomePersonagem[3]}`)
 filme.elenco[0]= "Xuxa"
 console.log(filme)
